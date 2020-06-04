dc497f6007d4048aa73f90ffbf6106b55bc569e2
konyan <devkonyan@gmail.com>
2020-06-04 23:05:25 +0630

    add more test

--- reverseString.test.js
+++ reverseString.test.js
@@ -3,3 +3,7 @@ const reverseString = require('./reverseString');
 test('Should be reverse String', () => {
   expect(reverseString('HELLO')).toBe('OLLEH');
 });
+
+test('Should be NOT reverse String', () => {
+  expect(reverseString('HELLO')).not.toBe('HELLO');
+});
