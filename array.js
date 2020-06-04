1aa82c9fac730e084a954c662593ffa400c95222
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-06-04 20:32:08 +0200

    array function and test

---
+++ array.js
@@ -0,0 +1,14 @@
+function analyze(array) {
+    let average = array.reduce((prev, cur) => prev + cur, 0) / array.length;
+    let min = array.reduce((prev, cur) => prev < cur ? prev : cur);
+    let max = array.reduce((prev, cur) => prev > cur ? prev : cur);
+    let length = array.length;
+
+    return {
+        average,
+        min,
+        max,
+        length,
+    };
+}
+module.exports = analyze;
