e264d8f92713ac8452692118c373cb7434b7c2f0
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-06-04 20:10:28 +0200

    cipher test

--- cipher.js
+++ cipher.js
@@ -1,9 +1,9 @@
 const cipher = string => string.replace(/([a-z])/ig, (char => {
     let charNumber = char.charCodeAt(0)
-    charNumber === 122 //'z'
-        ? charNumber = 97 // 'Z'
-        : charNumber === 90 //'z' => 'a'
-            ? charNumber = 65 //'Z' => 'A'
+    charNumber === 122
+        ? charNumber = 97
+        : charNumber === 90
+            ? charNumber = 65
             : charNumber += 1
     return String.fromCharCode(charNumber)
 }));
