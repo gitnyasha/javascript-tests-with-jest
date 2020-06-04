67c59d11eba476550b79efbc156c45479e43b718
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-06-04 21:16:44 +0200

    linter errors

--- cipher.js
+++ cipher.js
@@ -1,6 +1,12 @@
 const cipher = (string) => string.replace(/([a-z])/gi, (char) => {
   let charNumber = char.charCodeAt(0);
-  charNumber === 122 ? (charNumber = 97) : charNumber === 90 ? (charNumber = 65) : (charNumber += 1);
+  if (charNumber === 122) {
+    charNumber = 97;
+  } else if (charNumber === 90) {
+    charNumber = 65;
+  } else {
+    charNumber += 1;
+  };
   return String.fromCharCode(charNumber);
 });
 module.exports = cipher;
